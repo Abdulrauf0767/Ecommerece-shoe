d797d8872ed2c5bd1cf6fa1e65ec3938eafd19d3
Abdulrauf0767 <araof1803@mail.com>
2025-05-04 13:56:02 +0500

    set main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import {BrowserRouter  } from "react-router-dom";
 import { Provider } from "react-redux";
-import store from './App/Store.js';
+import store from '../src/App/Store.js'
+import { BrowserRouter } from "react-router-dom";
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
