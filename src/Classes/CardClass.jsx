35711012c5e9fea2d3334f6a2e59270cdf9c5368
Abdulrauf0767 <araof1803@mail.com>
2025-05-04 14:48:08 +0500

    responsive card

--- src/Classes/CardClass.jsx
+++ src/Classes/CardClass.jsx
@@ -6,8 +6,8 @@ class Card extends React.Component {
     const { id, name, image, price, description } = this.props;
     return (
       <div className="bg-white rounded-lg shadow-md overflow-hidden border border-gray-200 hover:scale-105 transition-all duration-300">
-        <NavLink to={`/productdetails/${id}`} className="flex flex-col h-full items-start justify-between  pb-2">
-          <div className="w-full lg:h-48 h-[120px] bg-gray-100 flex items-center justify-center">
+        <NavLink to={`/productdetails/${id}`} className="flex flex-col h-[120px]  items-start justify-between  pb-2">
+          <div className="w-full lg:h-48 h-[50px] bg-gray-100 flex items-center justify-center">
             <img 
               src={image} 
               alt={name} 
@@ -15,12 +15,12 @@ class Card extends React.Component {
             />
           </div>
           <div className="pr-2 pl-2 h-fit">
-            <h3 className="md:text-lg text-[10px] font-semibold mb-2 line-clamp-2">{name}</h3>
-            <p className="text-gray-600 text-[8px] mb-3 line-clamp-2">
+            <h3 className="md:text-lg text-[8px] font-semibold mb-2 line-clamp-1 ">{name}</h3>
+            <p className="text-gray-600 text-[8px] mb-3 line-clamp-2 hidden md:block">
               {description}
             </p>
             <div className="flex flex-col h-fit md:flex-row justify-between items-start">
-              <span className="font-bold lg:text-lg text-xs">${price}</span>
+              <span className="font-bold lg:text-lg text-[9px]">${price}</span>
               <button className="bg-blue-500 hover:bg-blue-600 text-white px-3 py-1 rounded md:text-sm text-[5px]">
                 View Details
               </button>
