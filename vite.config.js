2a3d1791f5a87cf3aa7fdef574a6992e7295b2da
Abdulrauf0767 <araof1803@mail.com>
2025-05-05 14:21:24 +0500

    icons issue

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    include: ['react-icons/fa']
+  build: {
+    rollupOptions: {
+      external: ['react-icons/fa']
+    }
   }
-})
+});
