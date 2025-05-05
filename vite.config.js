ab0d4044b85015456c173dae6d2e66f84b7cdec3
Abdulrauf0767 <araof1803@mail.com>
2025-05-05 14:49:18 +0500

    remove

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,4 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: (id) => {
-        return id.includes('react-icons/fa') || id.includes('node_modules');
-      }
-    }
-  },
-  optimizeDeps: {
-    include: ['react-icons/fa']
-  }
-});
+  });
