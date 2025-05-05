6442123500aa3b7d6ad5b6a66d7deb2ff5385940
Abdulrauf0767 <araof1803@mail.com>
2025-05-05 15:51:57 +0500

    gap

--- src/Components/Cards.jsx
+++ src/Components/Cards.jsx
@@ -23,7 +23,7 @@ const Cards = () => {
     <div className="container mx-auto px-4 py-8 w-full max-w-7xl">
       <h1 className="text-3xl font-bold text-center mb-4">Products</h1>
       <p className=' mb-4 w-[90%] justify-self-center md:w-[50%] leading-0  text-justify'>Explore our wide range of trendy products.Discover exclusive deals on fashion, electronics & more.Secure payments & fast delivery at your fingertips.</p>
-      <div className="grid grid-cols-2 xs:grid-cols-2 sm:grid-cols-4 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-4 gap-3 sm:gap-4 md:gap-5 lg:gap-6">
+      <div className="grid grid-cols-2 xs:grid-cols-2 sm:grid-cols-4 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-4 gap-3 gap-y-6 sm:gap-4 md:gap-5 lg:gap-6">
         {Array.isArray(list) && list.map((product) => (
           <Card
             key={product.id}
